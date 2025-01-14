0dd32b55aafa9374e55a356dc753439ab390cfd6
Serg Sh. <vampir6307@gmail.com>
2025-01-14 08:58:01 +0300

    CORRECTION140125getenv

--- src/test/java/config/BookStoreConfig.java
+++ src/test/java/config/BookStoreConfig.java
@@ -2,9 +2,9 @@ package config;
 
 public class BookStoreConfig {
     public static String getUserName(){
-        return System.getProperty("USERNAME");
+        return System.getenv("USERNAME");
     }
     public static String getPassword(){
-        return System.getProperty("PASSWORD");
+        return System.getenv("PASSWORD");
     }
 }
