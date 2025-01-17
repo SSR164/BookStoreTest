db77f79e05e4b894baec28cdeaaaff93a80c6f1c
Serg Sh. <vampir6307@gmail.com>
2025-01-17 07:57:27 +0300

    CORRECTION170125

--- src/test/java/config/BookStoreConfig.java
+++ src/test/java/config/BookStoreConfig.java
@@ -2,7 +2,7 @@ package config;
 
 public class BookStoreConfig {
     public static String getUserName(){
-        return System.getProperty("username");
+        return System.getProperty("userName");
     }
     public static String getPassword(){
         return System.getProperty("password");
